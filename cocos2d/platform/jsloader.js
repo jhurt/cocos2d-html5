d948907cfe7fd468e510438d45d4b8e8811374e5
Jason Hurt <jleehurt@gmail.com>
2013-10-16 19:01:35 -0700

    remove loading indicator

--- cocos2d/platform/jsloader.js
+++ cocos2d/platform/jsloader.js
@@ -246,27 +246,6 @@
 
     }
 
-    var loadJsImg = document.getElementById("cocos2d_loadJsImg");
-    if(!loadJsImg){
-        loadJsImg = new Image();
-        loadJsImg.src = "data:image/gif;base64,R0lGODlhEAAQALMNAD8/P7+/vyoqKlVVVX9/fxUVFUBAQGBgYMDAwC8vL5CQkP///wAAAP///wAAAAAAACH/C05FVFNDQVBFMi4wAwEAAAAh+QQFAAANACwAAAAAEAAQAAAEO5DJSau9OOvNex0IMnDIsiCkiW6g6BmKYlBFkhSUEgQKlQCARG6nEBwOgl+QApMdCIRD7YZ5RjlGpCUCACH5BAUAAA0ALAAAAgAOAA4AAAQ6kLGB0JA4M7QW0hrngRllkYyhKAYqKUGguAws0ypLS8JxCLQDgXAIDg+FRKIA6v0SAECCBpXSkstMBAAh+QQFAAANACwAAAAACgAQAAAEOJDJORAac6K1kDSKYmydpASBUl0mqmRfaGTCcQgwcxDEke+9XO2WkxQSiUIuAQAkls0n7JgsWq8RACH5BAUAAA0ALAAAAAAOAA4AAAQ6kMlplDIzTxWC0oxwHALnDQgySAdBHNWFLAvCukc215JIZihVIZEogDIJACBxnCSXTcmwGK1ar1hrBAAh+QQFAAANACwAAAAAEAAKAAAEN5DJKc4RM+tDyNFTkSQF5xmKYmQJACTVpQSBwrpJNteZSGYoFWjIGCAQA2IGsVgglBOmEyoxIiMAIfkEBQAADQAsAgAAAA4ADgAABDmQSVZSKjPPBEDSGucJxyGA1XUQxAFma/tOpDlnhqIYN6MEAUXvF+zldrMBAjHoIRYLhBMqvSmZkggAIfkEBQAADQAsBgAAAAoAEAAABDeQyUmrnSWlYhMASfeFVbZdjHAcgnUQxOHCcqWylKEohqUEAYVkgEAMfkEJYrFA6HhKJsJCNFoiACH5BAUAAA0ALAIAAgAOAA4AAAQ3kMlJq704611SKloCAEk4lln3DQgyUMJxCBKyLAh1EMRR3wiDQmHY9SQslyIQUMRmlmVTIyRaIgA7";
-
-        var canvasNode = document.getElementById(c.tag);
-        canvasNode.style.backgroundColor = "black";
-        canvasNode.parentNode.appendChild(loadJsImg);
-        
-        var canvasStyle = getComputedStyle?getComputedStyle(canvasNode):canvasNode.currentStyle;
-        loadJsImg.style.left = canvasNode.offsetLeft + (parseFloat(canvasStyle.width) - loadJsImg.width)/2 + "px";
-        loadJsImg.style.top = canvasNode.offsetTop + (parseFloat(canvasStyle.height) - loadJsImg.height)/2 + "px";
-        loadJsImg.style.position = "absolute";
-    }
-    
-    var updateLoading = function(p){
-        if(p>=1) {
-            loadJsImg.parentNode.removeChild(loadJsImg);
-        }
-    };
-
     var loaded = 0;
     var que = engine.concat(c.appFiles);
     if (navigator.userAgent.indexOf("Trident/5") > -1) {
@@ -279,12 +258,10 @@
                 f.src = que[i];
                 f.addEventListener('load',function(){
                     loadNext();
-                    updateLoading(loaded / que.length);
                     this.removeEventListener('load', arguments.callee, false);
                 },false);
                 d.body.appendChild(f);
             }
-            updateLoading(i / (que.length - 1));
         };
         loadNext();
     }
@@ -295,7 +272,6 @@
             s.src = f;
             s.addEventListener('load',function(){
                 loaded++;
-                updateLoading(loaded / que.length);
                 this.removeEventListener('load', arguments.callee, false);
             },false);
             d.body.appendChild(s);
